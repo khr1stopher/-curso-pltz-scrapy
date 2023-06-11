9f1928fd62c47e47e97c86f33e17eeba0593e151
Khristopher Pineda <kkromans009@gmail.com>
2023-06-11 13:28:48 -0700

    cap/11

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
@@ -3,14 +3,36 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
+    # TITLE = '//h1/a/text()'
+    # CITAS = '//span[@class="text" and @itemprop="text"]/text()'
+    # TOP_TEN_TAGS = '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()'
+
     start_urls = [
         'http://quotes.toscrape.com'
     ]
 
     def parse(self, response, **kwargs):
         print('*' * 10)
-        print('\n\n')
-        print(response.status)
+        print('\n')
+
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
+
         print('*' * 10)
-        print('\n\n')
-        print(response.headers)
+        print('\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print('Citas: ')
+        for quote in quotes:
+            print(f' - {quote}')
+
+        print('*' * 10)
+        print('\n')
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        print('Top Ten tags: ')
+
+        for tags in top_ten_tags:
+            print(f' - {tags}')
+
+        print('*' * 10)
