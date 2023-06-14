ef49e8cd34ded133a105b53c99cc48171f4883f4
Khristopher Pineda <kkromans009@gmail.com>
2023-06-14 00:34:36 -0700

    cap/13 response.follow

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_spider.py
@@ -6,10 +6,16 @@ class QuotesSpider(scrapy.Spider):
     # TITLE = '//h1/a/text()'
     # CITAS = '//span[@class="text" and @itemprop="text"]/text()'
     # TOP_TEN_TAGS = '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()'
+    # NEXT_PAGE_BTN = '//ul[@class="pager"]/li[@class="next"]/a/@href'
 
     start_urls = [
         'http://quotes.toscrape.com'
     ]
+
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
 
     def parse(self, response, **kwargs):
         title = response.xpath('//h1/a/text()').get()
@@ -20,3 +26,7 @@ class QuotesSpider(scrapy.Spider):
             "quotes": quotes,
             "top_ten_tags": top_ten_tags
         }
+
+        next_btn = response.xpath('//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        if next_btn:
+            yield response.follow(next_btn, callback=self.parse)
